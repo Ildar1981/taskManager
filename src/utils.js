152c40dfdb5c0a477808b11488a57a8581eb7e1e
Фахриев Ильдар <i.fakhriev@marvel.ru>
2024-09-03 15:50:13 +0300

    added changes

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,23 @@
 export const getFromStorage = function (key) {
-    return JSON.parse(localStorage.getItem(key) || "[]");
-  };
-  
-  export const addToStorage = function (obj, key) {
-    const storageData = getFromStorage(key);
-    storageData.push(obj);
-    localStorage.setItem(key, JSON.stringify(storageData));
-  };
-  
-  export const generateTestUser = function (User) {
-    localStorage.clear();
-    const testUser = new User("login", "password");
-    User.save(testUser);
-  };
-  
-  export const getTasks = function () {
-    return getFromStorage("tasks");
-  };
+  return JSON.parse(localStorage.getItem(key) || "[]");
+};
+
+export const addToStorage = function (obj, key) {
+  const storageData = getFromStorage(key);
+  storageData.push(obj);
+  localStorage.setItem(key, JSON.stringify(storageData));
+};
+
+export const replaceStorage = function (obj, key) {
+  localStorage.setItem(key, JSON.stringify(obj));
+};
+
+export const generateTestUser = function (User) {
+  localStorage.clear();
+  const testUser = new User("test", "test123");
+  User.save(testUser);
+};
+
+export const getTasks = function () {
+  return getFromStorage("tasks");
+};
